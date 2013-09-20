c63504b3103c459358d5f29d07413f84d6daa490
Jon Bardin <pair+jon@mavenlink.com>
2013-09-20 14:19:34 -0700

    put back in strategy

--- strategy.rb
+++ strategy.rb
@@ -1,5 +1,18 @@
 # crazy like a fox strategy
 
 on_turn do
-  'f'
+
+  first_player = visible_players.first || opponents.first
+  if first_player
+    run = move_away_from!(visible_players.first)
+    charge = move_towards!(visible_players.first)
+    if charge
+      return charge
+    elsif run
+      return run
+    end
+  end
+
+  return first_possible_move 'nesw'
+
 end
