cf34116de7f292a886aa1e6fbd12545e8b5bdedb
Jon Bardin <pair+jon@mavenlink.com>
2013-09-20 13:05:00 -0700

    go north

--- strategy.rb
+++ strategy.rb
@@ -1,11 +1,12 @@
 # crazy like a fox strategy
 
 on_turn do
-  run = move_away_from!(visible_players.first)
-  charge = move_towards!(visible_players.first)
-  if can_move?(run)
-    run
-  else
-    charge
-  end
+  #run = move_away_from!(visible_players.first)
+  #charge = move_towards!(visible_players.first)
+  #if can_move?(run)
+  #  run
+  #else
+  #  charge
+  #end
+  'n'
 end
