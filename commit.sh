55de69327e6c09d4de9c7685487edf0fbd502e45
Jon Bardin <pair+jon@mavenlink.com>
2013-09-20 14:09:11 -0700

    back to basics

---
+++ commit.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+git commit -a && git push origin master && curl 'http://kablammo.io/strategies/523c979a421aa95b5500e27f/pull?'
--- strategy.rb
+++ strategy.rb
@@ -1,22 +1,5 @@
 # crazy like a fox strategy
 
 on_turn do
-=begin
-  first_player = visible_players.first || opponents.first
-  if first_player
-    run = move_away_from!(visible_players.first)
-    charge = move_towards!(visible_players.first)
-    if charge
-      return charge
-    elsif run
-      return run
-    end
-  end
-=end
-#  if rand > 0.5
-    'f'
-#  else
-#    return 'n'
-#    #return first_possible_move 'nesw'
-#  end
+  'f'
 end
