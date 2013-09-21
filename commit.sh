386191a02eca5214a6eac3cdc07eb420af62e14c
Jon Bardin <pair+jon@mavenlink.com>
2013-09-20 17:55:39 -0700

    Fri Sep 20 17:55:39 PDT 2013

--- commit.sh
+++ commit.sh
@@ -6,4 +6,5 @@ git push origin master
 sleep 10
 curl 'http://kablammo.io/strategies/523c979a421aa95b5500e27f/pull?'
 sleep 10
-curl 'http://kablammo.io/battles' -H 'Content-Type: application/x-www-form-urlencoded' --data 'player1=523c979a421aa95b5500e27f&player2=523cb400421aa92dc204c342&player3=523b8af7421aa90b76000001&player4=523cbc59421aa92dc20aca5f' --compressed -i | grep Location
+#curl 'http://kablammo.io/battles' -H 'Content-Type: application/x-www-form-urlencoded' --data 'player1=523c979a421aa95b5500e27f&player2=523cb400421aa92dc204c342&player3=523b8af7421aa90b76000001&player4=523cbc59421aa92dc20aca5f' --compressed -i | grep Location
+curl 'http://kablammo.io/battles' -H 'Content-Type: application/x-www-form-urlencoded' --data 'player1=523c979a421aa95b5500e27f&player2=523cb400421aa92dc204c342&player3=&player4=523cbc59421aa92dc20aca5f' --compressed -i | grep Location
